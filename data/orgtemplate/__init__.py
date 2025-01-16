a31ca95ffa29db209fc4108b9360e09666d0182c
Sudhakar Rayavaram <sudhakar@tarkalabs.com>
2025-01-16 20:10:43 +0530

    feat: github action addtion and bug fix
    1. Changed org_template folder name to orgtemplate
    2. Added a github action to create the pdf when profile markdown changes

--- data/orgtemplate/__init__.py
+++ data/orgtemplate/__init__.py
@@ -5,5 +5,5 @@ from rendercv.themes.options import ThemeOptions, theme_options_theme_field_info
 theme_options_theme_field_info.default = "classic"
 
 
-class TarkalabsThemeOptions(ThemeOptions):
-    theme: Literal["tarkalabs"] = theme_options_theme_field_info
+class OrgtemplateThemeOptions(ThemeOptions):
+    theme: Literal["orgtemplate"] = theme_options_theme_field_info
